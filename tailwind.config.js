af9cb4ab2ae7c5a64e60353db268fc4e279353c2
rashed-mia <rashedkamal2565@gmail.com>
2024-12-12 08:26:21 +0600

    create a simple html template with taiwind ffrom figma file.

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#0F0E0E0D',
+        'secondary': '#0F0E0E33',
+        'brand': "#fc6320",
+        'light':"#FC63201A"
+       },
+    }
+  },
+  plugins: [],
+}
+
